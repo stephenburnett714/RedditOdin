35131bc977d263f42103dc9811dc612e103313f1
Stephen M. Burnett <stephen.m.burnett@uscis.dhs.gov>
2023-07-24 17:58:55 -0400

    created user sucessfully

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,20 +1,22 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-    respond_to :json
-    private
-    def respond_with(resource, _opts = {})
-        register_success && return if resource.persisted?
-        register_failed
-    end
+  respond_to :json
 
-    def register_success
-        render json: {
-            message: 'Signed up sucessfully.',
-            user: current_user
-        }, status: :ok
-    end
+  private
 
-    def register_failed
-        render json: { message: "Something went wrong." }, status: :unprocessable_entity
-    end
+  def respond_with(resource, _opts = {})
+    register_success && return if resource.persisted?
 
+    register_failed
+  end
+
+  def register_success
+    render json: {
+      message: 'Signed up Sucessfully.',
+      user: current_user
+    }, status: :ok
+  end
+
+  def register_failed
+    render json: {message: 'Something went wrong.'}, status: :unprocessable_entity
+  end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,24 +1,24 @@
 class Users::SessionsController < Devise::SessionsController
-    respond_to :json
-    private
-    def respond_with(_resource, _opts = {})
-        render json: {
-            message: 'You are logged in.',
-            user: current_user
-        }, status: :ok
-    end
+  respond_to :json
+  private
+  def respond_with(resource, _opts = {})
+    rrender json: {
+      message: 'You are logged in.',
+      user: current_user
+    }, status: :ok
+  end
 
-    def respond_to_on_destroy
-        log_out_success && return if current_user
+  def respond_to_on_destroy
+    log_out_success && return if current_user
 
-        log_out_failure
-    end
+    log_out_failure
+  end
 
-    def log_out_success
-        render json: {message: "You are logged out." }, status: :ok
-    end
+  def log_out_success
+    render json: {message: 'You are logged out.'}, status: :ok
+  end
 
-    def log_out_failure
-        render json: {message: "Nothing happened."}, status: :unauthorized
-    end
+  def log_out_failure
+    render json: {message: 'Nothing happend.'}, status: :unauthorized
+  end
 end
